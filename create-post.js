6b59990e52270e1947c31163e62c5397069c1046
mia.j.lawrence <mchaulawrence@gmail.com>
2023-07-28 14:32:16 -0700

    it works!!! the array works!!!

--- create-post.js
+++ create-post.js
@@ -1,14 +1,13 @@
-let posts = [
-    // {
-    // "Image":"mary-lawrence.jpg",
-    // "Title":"Mary",
-    // "Name":"Mia",
-    // "Date": "today",
-    // "Post": "How are yall?",
-    // },
-];
-
 (function () {
+    let posts = [
+        // {
+        // "Image":"mary-lawrence.jpg",
+        // "Title":"Mary",
+        // "Name":"Mia",
+        // "Date": "today",
+        // "Post": "How are yall?",
+        // },
+    ];
 
     let collectHtml = '';
 
@@ -97,56 +96,16 @@ let posts = [
             console.log("post loaded");
 
             document.getElementById('new-post').innerHTML = postHtml;
-            localStorage.setItem("myPosts", posts);
+            localStorage.setItem("myPosts", JSON.stringify(posts));
         });   
 
         console.log(posts);  
         document.getElementById('collect-post-info').innerHTML = collectHtml;
-    }
-    const news = localStorage.getItem("myPosts");
-    collectUserPost(); //collect user post
-
-    //load posts to news.html
-    // document.getElementById('load-posts').addEventListener('click', function (event) {
-        console.log("we getting there! " + news);   
-        //render blog post
-        function renderBlog (blogBoi) {
-            let blogHtml = '';
-            // blogBoi.sort((a,b) => {
-            //     if (a.Date_value < b.Date_value) { return 1; }
-            //     if (a.Date_value > b.Date_value) { return -1; }
-            //     return 0;
-            // });
-            
-            console.log("Preparing to create blog");
-            for (let blog of blogBoi) {
-                let blogContent = '';
-
-                let blogTemplate = `
-                    <div class="main-enclose">
-                        <div class="main-card">
-                            <img src="${blog["Image"]}" alt="Image not available" style="width:100%;">
-                            <div class="m-container">
-                                <h3>${blog["Title"]}</h3>
-                                <h5>${blog["Date"]}</span></h5>
-                                <p>${blog["Post"]}</p>
-                            </div>
-                        </div>
-                        <div class="bottom-card">
-                            <div class="container">
-                                <button class="button1">READ MORE &raquo;</button>
-                            </div>
-                        </div>
-                    </div>
-                    `;
-                blogHtml += blogTemplate;   
-                console.log("created blog");
-            }       
-            document.getElementById('blog-post').innerHTML = blogHtml;  
-        }
-    //  })
-    renderBlog(news);
-    console.log("blog rendered");
         
+        //saving posts to local storage
+        // const news = localStorage.getItem("myPosts");
+        // console.log("NEWS:" + news);
+    }
+    collectUserPost(); //collect user post
 
 }());
---
+++ news.js
@@ -0,0 +1,47 @@
+(function () {
+    const news = JSON.parse(localStorage.getItem("myPosts"));
+    //load posts to news.html
+    // document.getElementById('load-posts').addEventListener('click', function (event) {
+    // console.log("we getting there! " + news);   
+    //render blog post
+    function renderBlog (blogBoi) {
+        let blogHtml = '';
+        // blogBoi.sort((a,b) => {
+        //     if (a.Date_value < b.Date_value) { return 1; }
+        //     if (a.Date_value > b.Date_value) { return -1; }
+        //     return 0;
+        // });
+        
+        console.log("Preparing to create blog");
+        for (let blog of blogBoi) {
+            let blogContent = '';
+
+            let blogTemplate = `
+                <div class="main-enclose">
+                    <div class="main-card">
+                        <img src="${blog["Image"]}" alt="Image not available" style="width:100%;">
+                        <div class="m-container">
+                            <h3>${blog["Title"]}</h3>
+                            <h5>${blog["Date"]}</span></h5>
+                            <p>${blog["Post"]}</p>
+                        </div>
+                    </div>
+                    <div class="bottom-card">
+                        <div class="container">
+                            <button class="button1">READ MORE &raquo;</button>
+                        </div>
+                    </div>
+                </div>
+                `;
+            blogHtml += blogTemplate;   
+            console.log("created blog");
+        }       
+        document.getElementById('blog-post').innerHTML = blogHtml;  
+    }
+    //  })
+    renderBlog(news);
+    console.log("blog rendered");
+
+
+    }());
+    
